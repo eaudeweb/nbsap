f2144b6c563e464d7e58470adde25d91601d143c
mihait <mihait@eaudeweb.(none)>
2012-08-08 17:26:22 +0300

    Add page containing full description for each indicator.

--- nbsap/indicators.py
+++ nbsap/indicators.py
@@ -9,11 +9,28 @@ def initialize_app(app):
     app.register_blueprint(indicators)
 
 @indicators.route('/indicators')
-@sugar.templated("indicators_listing.html")
+@sugar.templated("indicators/indicators_listing.html")
 def list_indicators():
     aichi_indicators = _load_json("../refdata/aichi_indicators.json")
     return {
             "indicators": aichi_indicators
+           }
+
+@indicators.route("/indicators/<int:indicator_id>")
+@sugar.templated("indicators/view.html")
+def view(indicator_id):
+    aichi_indicators = _load_json("../refdata/aichi_indicators.json")
+    aichi_indicator_keys = _load_json("../refdata/aichi_indicator_keys.json")
+
+    # Online targets are 1-indexed but in json database it is 0-indexed
+    correct_index = int(indicator_id)-1
+    specified_indicator = aichi_indicators[correct_index]
+    # Remove 'id' key as it has no correspondence in transit_dict
+    del specified_indicator['id']
+
+    return {
+            "indicator": specified_indicator,
+            "transit_dict": aichi_indicator_keys
            }
 
 
