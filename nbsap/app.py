f399f4e3e4585efe0b2a3fac2829d2b54d21b5cf
Camelia Groza <camelia.groza@gmail.com>
2012-08-13 17:05:33 +0300

    Add mongodb base config.

--- nbsap/app.py
+++ nbsap/app.py
@@ -1,5 +1,6 @@
 import flask
 from flaskext.babel import Babel
+from flask.ext.pymongo import PyMongo
 
 import goals
 import targets
@@ -8,8 +9,10 @@ import objectives
 import actions
 
 default_config = {
+        "MONGO_DBNAME": "nbsap",
 }
 
+mongo = PyMongo()
 babel = Babel()
 
 def create_app(instance_path=None):
@@ -17,6 +20,7 @@ def create_app(instance_path=None):
                       instance_path=instance_path,
                       instance_relative_config=True)
 
+    app.config.update(default_config)
     app.config.from_pyfile("settings.py", silent=True)
 
     goals.initialize_app(app)
@@ -25,6 +29,7 @@ def create_app(instance_path=None):
     objectives.initialize_app(app)
     actions.initialize_app(app)
 
+    mongo.init_app(app)
     babel.init_app(app)
 
     @app.route('/ping')
