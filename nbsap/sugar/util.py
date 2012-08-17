42374e1e65d40315d6a370100d7576c1800eddf0
Mihai Tabara <tabara.mihai@gmail.com>
2012-08-17 11:43:04 +0300

    Add basic widgets.

--- nbsap/sugar/util.py
+++ nbsap/sugar/util.py
@@ -1,6 +1,7 @@
 from functools import wraps
 
 import flask
+import flatland.out.markup
 
 def templated(template=None):
     def decorator(f):
@@ -17,4 +18,37 @@ def templated(template=None):
         decorated_function.not_templated = f
         return decorated_function
     return decorator
+
+class MarkupGenerator(flatland.out.markup.Generator):
+
+    def __init__(self, template):
+        super(MarkupGenerator, self).__init__("html")
+        self.template = template
+
+    def children_order(self, field):
+        if isinstance(field, flatland.Mapping):
+            return [kid.name for kid in field.field_schema]
+        else:
+            return []
+
+    def widget(self, element, widget_name=None):
+        if widget_name is None:
+            widget_name = element.properties.get("widget", "input")
+        widget_macro = getattr(self.template.module, widget_name)
+        return widget_macro(self, element)
+
+    def properties(self, field, id=None):
+        properties = {}
+
+        if id:
+            properties["id"] = id
+        if field.properties.get("css_class", None):
+            properties["class"] = field.properties["css_class"]
+        if not field.optional:
+            properties["required"] = ""
+            if "not_empty_error" in field.properties:
+                properties["title"] = field.properties["not_empty_error"]
+        if field.properties.get("attr", None):
+            properties.update(field.properties["attr"])
+        return properties
 
