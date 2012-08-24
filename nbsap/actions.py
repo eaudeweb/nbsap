be52f18c45bb23dc200364ca3b3bb794c4759f56
Mihai Tabara <tabara.mihai@gmail.com>
2012-08-24 19:06:08 +0300

    Add (admin) national actions (edit + listing)

--- nbsap/actions.py
+++ nbsap/actions.py
@@ -1,3 +1,4 @@
+import schema
 import flask
 import sugar
 from database import mongo
@@ -24,3 +25,42 @@ def view(objective_id, subobj_id):
                 "action": action
            }
 
+@actions.route("/objectives/<int:objective_id>/<int:subobj_id>/action/edit", methods=["GET", "POST"])
+@sugar.templated("actions/edit.html")
+def edit(objective_id, subobj_id):
+
+    # sub-objectives are 0-indexed
+    subobj_id -= 1
+
+    action = mongo.db.actions.find_one_or_404({'id': objective_id})
+    action_schema = schema.Action(action)
+
+    # default display language is English
+    try:
+        selected_language = flask.request.args.getlist('lang')[0]
+    except IndexError:
+        selected_language = u'en'
+
+    app = flask.current_app
+
+    if flask.request.method == "POST":
+        data = flask.request.form.to_dict()
+
+        selected_language = data['language']
+
+        action_schema['actions'][subobj_id]['body'][selected_language].set(data['body-' + selected_language])
+
+        if action_schema.validate():
+
+            action['actions'][subobj_id]['body'][selected_language] = data['body-' + selected_language]
+
+            flask.flash("Saved changes.", "success")
+            mongo.db.actions.save(action)
+
+    return {
+                "language": selected_language,
+                "action": action,
+                "schema": action_schema,
+                "objective_id": objective_id,
+                "subobj_id": subobj_id
+    }
