edd13edbc1eb9aa33c4f5b992a1770a9778e1b2f
Camelia Groza <camelia.groza@gmail.com>
2012-08-27 17:27:37 +0300

    Update indicator schema.

--- nbsap/schema/aichi.py
+++ nbsap/schema/aichi.py
@@ -2,7 +2,7 @@ import flatland
 
 from common import I18nString, CommonString, CommonEnum, CommonList, ListValue,\
                    GoalEnumValue
-from .refdata import goals, targets, mapping
+from .refdata import goals, targets, mapping, indicator_data
 
 _GoalSchemaDefinition = flatland.Dict.of(
             CommonString.named('short_title'),
@@ -12,24 +12,70 @@ _GoalSchemaDefinition = flatland.Dict.of(
         )
 
 _IndicatorSchemaDefinition = flatland.Dict.of(
-            CommonString.named('status'),
-            CommonString.named('classification'),
-            CommonString.named('scale'),
-            CommonString.named('other_targets'),
-            CommonString.named('goal'),
-            CommonString.named('relevant_target'),
-            CommonString.named('sources'),
-            CommonString.named('sensitivity'),
-            CommonString.named('question'),
-            CommonString.named('links'),
-            CommonString.named('validity'),
-            CommonString.named('measurer'),
-            CommonString.named('sub_indicator'),
-            CommonString.named('head_indicator'),
-            CommonString.named('ease_of_communication'),
-            CommonString.named('requirements'),
+            I18nString.named('status')
+                .with_properties(widget="edit_textarea")
+                .using(label="Status of development"),
+            I18nString.named('classification')
+                .with_properties(widget="edit_textarea")
+                .using(label="Operational Classification"),
+            CommonEnum.named('scale')
+                .valued(*sorted(indicator_data['scale']))
+                .with_properties(widget="select")
+                .using(label="Scale (global, regional, national, sub-national)"),
+            CommonList.named('other_targets')
+                .of(CommonString.named('other_targets'))
+                .including_validators(ListValue())
+                .using(label="Other Relevant Aichi Targets")
+                .with_properties(widget="list",
+                                 valid_values=targets.keys(),
+                                 value_labels=targets,
+                                 css_class="chzn-select",
+                                 multiple="multiple"),
+            CommonEnum.named('goal')
+                .valued(*sorted(goals.keys()))
+                .using(label="Strategic Goal")
+                .with_properties(widget="select", value_label=goals),
+            CommonEnum.named('relevant_target')
+                .including_validators(GoalEnumValue())
+                .valued(*sorted(targets.keys()))
+                .with_properties(widget="select", value_label=targets)
+                .using(label="Most Relevant Aichi Target"),
+            I18nString.named('sources')
+                .with_properties(widget="edit_textarea")
+                .using(label="Data Sources"),
+            CommonEnum.named('sensitivity')
+                .valued(*sorted(indicator_data['sensitivity']))
+                .with_properties(widget="select")
+                .using(label="Sensitivity (can it be used to make assessment by 2015?)"),
+            I18nString.named('question')
+                .with_properties(widget="edit_textarea")
+                .using(label="Communication Question"),
+            CommonList.named('links').of(CommonString.named('links'))
+                .using(label="Related Links"),
+            CommonEnum.named('validity')
+                .valued(*sorted(indicator_data['validity']))
+                .with_properties(widget="select")
+                .using(label="Scientific Validity"),
+            I18nString.named('measurer')
+                .with_properties(widget="edit_textarea")
+                .using(label="Who's responsible for measuring?"),
+            I18nString.named('sub_indicator')
+                .with_properties(widget="edit_textarea")
+                .using(label="Indicator Sub-topics"),
+            I18nString.named('head_indicator')
+                .with_properties(widget="edit_textarea")
+                .using(label="Headline Indicator"),
+            CommonEnum.named('ease_of_communication')
+                .valued(*sorted(indicator_data['ease_of_communication']))
+                .with_properties(widget="select")
+                .using(label="How easy can it be communicated?"),
+            I18nString.named('requirements')
+                .with_properties(widget="edit_textarea")
+                .using(label="Data Requirements"),
             CommonString.named('id'),
-            CommonString.named('name'),
+            I18nString.named('name')
+                .with_properties(widget="edit_textarea")
+                .using(label="Operational Indicator")
 
         )
 
