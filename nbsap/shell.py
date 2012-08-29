c21e837f83a231b359de980614db781ed14c8644
Camelia Groza <camelia.groza@gmail.com>
2012-08-29 17:25:50 +0300

    Add shell command for fixing the scale field.

--- nbsap/shell.py
+++ nbsap/shell.py
@@ -41,12 +41,26 @@ def indicator_link_list_to_dict():
 
         mongo.db.indicators.save(indicator)
 
+def split_scale_in_list():
+
+    indicators = mongo.db.indicators.find()
+
+    for indicator in indicators:
+
+        if indicator.get('scale'):
+            indicator['scale'] = indicator['scale'].split(', ')
+
+        mongo.db.indicators.save(indicator)
+
+
 def update_indicators():
     add_language_fields_to_indicators()
     indicator_link_list_to_dict()
+    split_scale_in_list()
 
 extra_shell_context = {
     "add_language_fields_to_indicators": add_language_fields_to_indicators,
     "indicator_link_list_to_dict": indicator_link_list_to_dict,
-    "update_indicators": update_indicators
+    "update_indicators": update_indicators,
+    "split_scale_in_list": split_scale_in_list
 }
