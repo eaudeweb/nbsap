9a3d5dab99863825976259512aea9232729a4822
Camelia Groza <camelia.groza@gmail.com>
2012-09-25 12:49:12 +0300

    Refactor markdown editor setup.

--- nbsap/static/js/edit_markdown.js
+++ nbsap/static/js/edit_markdown.js
@@ -2,32 +2,32 @@ $(function() {
   app.converter = Markdown.getSanitizingConverter();
 
   app.en_editor = new Markdown.Editor(app.converter, '-en');
-  app.fr_editor = new Markdown.Editor(app.converter, '-fr');
-  app.nl_editor = new Markdown.Editor(app.converter, '-nl');
-
   app.en_editor.run();
-  app.fr_editor.run();
-  app.nl_editor.run();
-
   $('#wmd-preview-en').hide();
-  $('#wmd-preview-fr').hide();
-  $('#wmd-preview-nl').hide();
-
   $('#wmd-quote-button-en').remove();
-  $('#wmd-quote-button-fr').remove();
-  $('#wmd-quote-button-nl').remove();
-
   $('#wmd-code-button-en').remove();
-  $('#wmd-code-button-fr').remove();
-  $('#wmd-code-button-nl').remove();
-
   $('#wmd-image-button-en').remove();
-  $('#wmd-image-button-fr').remove();
-  $('#wmd-image-button-nl').remove();
-
   $('#wmd-hr-button-en').remove();
-  $('#wmd-hr-button-fr').remove();
-  $('#wmd-hr-button-nl').remove();
+
+  if($('#wmd-input-fr').length) {
+    app.fr_editor = new Markdown.Editor(app.converter, '-fr');
+    app.fr_editor.run();
+    $('#wmd-preview-fr').hide();
+    $('#wmd-quote-button-fr').remove();
+    $('#wmd-code-button-fr').remove();
+    $('#wmd-image-button-fr').remove();
+    $('#wmd-hr-button-fr').remove();
+  }
+
+  if($('#wmd-input-nl').length) {
+    app.nl_editor = new Markdown.Editor(app.converter, '-nl');
+    app.nl_editor.run();
+    $('#wmd-preview-nl').hide();
+    $('#wmd-quote-button-nl').remove();
+    $('#wmd-code-button-nl').remove();
+    $('#wmd-image-button-nl').remove();
+    $('#wmd-hr-button-nl').remove();
+  }
 
   $('.preview').appendTo("#wmd-button-row-en, #wmd-button-row-fr, #wmd-button-row-nl");
   $('.help').appendTo("#wmd-button-row-en, #wmd-button-row-fr, #wmd-button-row-nl");
