9933be4c3ca9ec15e31d8c233c7cd483b1619beb
Mihai Tabara <tabara.mihai@gmail.com>
2012-10-03 13:21:29 +0300

    Remove from under version control the scripts and database jsons.

--- scripts/mongoimport.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mongoimport -d nbsap -c goals --drop --jsonArray ../nbsap/refdata/aichi_goals.json
-mongoimport -d nbsap -c indicators --drop --jsonArray ../nbsap/refdata/aichi_indicators.json
-mongoimport -d nbsap -c targets --drop --jsonArray ../nbsap/refdata/aichi_targets.json
-mongoimport -d nbsap -c actions --drop --jsonArray ../nbsap/refdata/be_actions.json
-mongoimport -d nbsap -c objectives --drop --jsonArray ../nbsap/refdata/be_objectives.json
